5fd2b366cf1de26a3b4be25bf7776716bf866fec
Felix Zimmermann <91369763+felzim@users.noreply.github.com>
2021-11-20 15:55:36 +0100

    Layout elements and RandomUnderSampler

--- FakeNewsDetector.py
+++ FakeNewsDetector.py
@@ -1,23 +1,34 @@
 from src import config
 
+import pandas as pd
+import numpy as np
+
+# Reguläre Ausdrücke
 import re
 
+# Sklearn
 from sklearn.linear_model import PassiveAggressiveClassifier
-from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
+# Balancieren / Undersampling
+from imblearn.under_sampling import RandomUnderSampler
+
+# Metriken
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Dash-Komponenten
 import dash
 from dash import callback_context
 from dash.dependencies import Input, Output, State
 from dash import html
 from dash import dcc
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
+# NLTK
 from nltk.corpus import stopwords
-
 stopwords = stopwords.words('english')
 stopwords = set(stopwords)
+
 
 df_fake = pd.read_csv(config.DATA_FAKE)
 df_true = pd.read_csv(config.DATA_TRUE)
@@ -39,6 +50,12 @@ class FakeNewsDetector():
 
     def __init__(self):
 
+        self.last_result = 'init'
+        self.accuracy_score = None
+        self.precision_score = None
+        self.recall_score = None
+        self.f1_score = None
+
         # TFIDF Vectorizer initialisieren
         self.tfvect = TfidfVectorizer(stop_words='english', max_df=0.90)
 
@@ -54,8 +71,20 @@ class FakeNewsDetector():
         # Train und Test mit fit_transform in eine Matrix konvertieren / vektorisieren
         self.tfidf_x_train = self.tfvect.fit_transform(self.X_train.astype('U').values)
 
+        # Datensatz balancieren
+        rus = RandomUnderSampler()
+        self.tfidf_x_train_rs, self.y_train_rs = rus.fit_resample(self.tfidf_x_train, self.y_train)
+
         # Classifier trainieren
-        self.classifier.fit(self.tfidf_x_train, self.y_train)
+        self.classifier.fit(self.tfidf_x_train_rs, self.y_train_rs)
+
+        # Score berechnen
+        y_pred = self.classifier.predict(self.tfvect.transform(self.X_test))
+
+        self.accuracy_score = round(accuracy_score(self.y_test, y_pred),2)
+        self.precision_score = round(precision_score(self.y_test, y_pred),2)
+        self.recall_score = round(recall_score(self.y_test, y_pred),2)
+        self.f1_score = round(f1_score(self.y_test, y_pred),2)
 
 
     def detect_fake_news(self, news):
@@ -67,12 +96,13 @@ class FakeNewsDetector():
 
         # eingegebenen Text mit dem bereits gefitteten Modell in numerisches Format transformieren
         vectorized_input_data = self.tfvect.transform(input_data)
-        prediction = self.classifier2.predict(vectorized_input_data)[0]
+        prediction = self.classifier.predict(vectorized_input_data)[0]
 
         if prediction == 0:
-            return ["Fake", self.classifier2.decision_function(vectorized_input_data)[0]]
+            return ["Fake", self.classifier.decision_function(vectorized_input_data)[0]]
         if prediction == 1:
-            return ["True", self.classifier2.decision_function(vectorized_input_data)[0]]
+            return ["True", self.classifier.decision_function(vectorized_input_data)[0]]
+
 
     def overlap_training_data(self, news):
         ''' Funktion, die prüft, zu welchem Prozentsatz die Wörter eines Strings
@@ -105,7 +135,7 @@ class FakeNewsDetector():
         return round((count_enthalten / len(list_of_words) * 100), 2)
 
 
-    def process(self, df_true_= df_true, df_fake_=df_fake, dropwords=[], test_size=0.20):
+    def process(self, df_true_= df_true, df_fake_=df_fake, dropwords=[], test_size=0.25):
         ''' Funktion, die aus zwei Dataframes einen neuen zusammensetzt und diesen bereinigt
             Es kann optional eine Liste von Wörtern mit übergeben werden, die dann aus dem neuen DF herausgefiltert werden
         '''
@@ -117,13 +147,14 @@ class FakeNewsDetector():
         # neuen Dataframe mit true and fake
         df_ = pd.concat([df_true_, df_fake_])
 
-        # neuen Index erstellen und alten Index droppen
+        # neuen Index erstellen und alten Index droppen. Titel-Spalte ebenfalls entfernen
         df_ = df_.reset_index(drop=False)
-        df_ = df_.drop(columns='index')
+        df_ = df_.drop(columns=['index', 'title'])
+
+        self.X_raw = df_['text']
 
         # Sonderzeichen entfernen (alles, was nicht a-zA-Z# ist)
         df_['text'] = df_['text'].str.replace("[^a-zA-Z#]", " ", regex=True)
-
 
         # Alle Wörter, die länger als 3 Zeichen sind, entfernen
         df_['text'] = df_['text'].apply(lambda x: ' '.join([w for w in x.split() if len(w) > 3]))
@@ -141,8 +172,9 @@ class FakeNewsDetector():
         # alle stopwords entfernen
         df_['text'] = df_['text'].apply(lambda x: ' '.join([w for w in x.split() if w not in stopwords]))
 
-        # wenn eine Dropliste mitgegeben wurde, diese aus dem neuen DF entfernen
-        #df_['text'] = df_['text'].apply(lambda x: ' '.join([w for w in x.split() if w not in list_of_dropwords]))
+        # weitere Bereinigung
+        df_['text'] = df_['text'].str.replace("reuters", '')
+        df_['text'] = df_['text'].str.replace("washington", '')
 
         for word in dropwords:
             df_['text'] = df_['text'].str.replace(word, '')
@@ -153,7 +185,7 @@ class FakeNewsDetector():
         X = df_['text']
         y = df_['category']
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=test_size)
 
 
     # Web-Interface auf 127.0.0.1:8050
@@ -169,53 +201,116 @@ class FakeNewsDetector():
                 href='assets/custom_loader.css'
             ),
 
-            html.H2('Hi, I\'m a very simple Fake News Detector', style={'textAlign': 'center', 'margin-bottom': '2em'}),
 
             html.Div([
 
-                html.Button('Text prüfen', id='text-pruefen-button', n_clicks=0),
-                html.Button('Modell neu berechnen', id='model-button', style={'float': 'right'}, n_clicks=0),
-                html.Br(),
-                dcc.Textarea(
-                    id='textarea-status',
-                    value='Bitte einen englischsprachigen Text eingeben.',
-                    style={'width': '100%','height': 200, 'margin-top': '1em', 'padding': '4px'},
-                ),
+                    html.H2('Hi, I\'m a very simple Fake News Detector',
+                        style={'font-family': 'Georgia'}),
 
-                html.Div(id='textarea-status-output', style={'whiteSpace': 'pre-line',
-                                                             'font-size': '0.9em',
-                                                             'height': 150}),
 
-                html.H4("Optionen: "),
-                html.Span("Features aus Datensatz entfernen: "),
+                    html.P(
+                        style={'height': '1em'}
+                    ),
 
-                html.Br(),
-                dcc.Input(
-                    id='input-dropwords',
-                    placeholder='Wörter durch Komma getrennt eingeben...',
-                    type='text',
-                    value='',
-                    style={'width': '50%', 'margin-top': '1em'}
-                ),
-                html.Button('Feature processing ausführen', id='processing-button', n_clicks=0),
 
-            ],
-            style={'width': '40%'}),
-        ])
+                    html.Button('Text prüfen', id='text-pruefen-button', n_clicks=0),
+
+                    html.Br(),
+                    dcc.Textarea(
+                        id='textarea-status',
+                        value=self.X_raw[np.random.randint(len(self.X_raw))],
+                        style={'width': '100%','height': 200, 'margin-top': '1em', 'padding': '12px'},
+                    ),
+
+
+                    html.Div([
+
+
+                        html.Div(id='textarea-status-output',
+                                 style={
+                                'font-size': '0.9em'},
+                        ),
+
+                        html.Img(
+                            id="res-image",
+                            style={'width': '25px'},
+                        ),
+
+                    ],
+                    style={'margin-top': '0.2em', 'display': 'flex', 'flex-direction': 'row', 'justify-content': 'flex-end', 'align-items': 'center', 'gap': '15px'}),
+
+
+                    html.P(
+                        style={'clear': 'both'}
+                    ),
+
+                    html.H4("Optionen: "),
+                    html.Span("Features aus Datensatz entfernen: "),
+
+                    html.Br(),
+
+                    html.Div([
+
+                        dcc.Input(
+                                id='input-dropwords',
+                                placeholder='Wörter durch Komma getrennt eingeben...',
+                                type='text',
+                                value='',
+                                style={'width': '30%'}
+                        ),
+                        html.Button('Feature processing ausführen', id='processing-button', n_clicks=0),
+                        html.Button('Modell neu berechnen', id='model-button', n_clicks=0),
+
+                    ], style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'flex-start', 'align-items': 'stretch', 'gap':'10px', 'margin-top':'1em'}),
+
+
+                    html.Div(id='textarea-status-output2', style={
+                            'white-space': 'pre-line',
+                            'font-size': '0.9em',
+                            'margin-top': '1em',
+                            'height': 150},),
+
+
+
+
+                ],
+                style={'margin': '0 auto', 'text-align': 'left', 'width': '40%'}),
+
+
+        ],
+        style={'text-align': 'center'})
+
+
+
+        @app.callback(
+            Output("res-image", "src"),
+
+            Input('textarea-status-output', 'children'),
+        )
+        def update_res_image(textarea_value):
+
+            if self.last_result == 'True':
+                src = "assets/check.svg"
+                return src
+            elif self.last_result == 'Fake':
+                src = "assets/x.svg"
+                return src
+            elif self.last_result == 'init':
+                src = "assets/init.svg"
+                return src
+
+
 
         @app.callback(
 
             Output('textarea-status-output', 'children'),
 
-            [Input('text-pruefen-button', 'n_clicks'),
-             Input('processing-button', 'n_clicks'),
-             Input('model-button', 'n_clicks'),
-             Input('input-dropwords', 'value')],
+            Input('text-pruefen-button', 'n_clicks'),
 
             State('textarea-status', 'value')
 
         )
-        def update_output(btn1, btn2, btn3, dropwords_value, textarea_value):
+        def update_output(btn1, textarea_value):
 
             if len(textarea_value) > 0:
 
@@ -224,23 +319,51 @@ class FakeNewsDetector():
                 if 'text-pruefen-button' in changed_id:
 
                     detection = self.detect_fake_news(textarea_value)
+                    self.last_result = detection[0]
                     overlap = self.overlap_training_data(textarea_value)
 
-                    return  f'\n\n Klassifiziert als: {detection[0]}' \
-                            f'\n\n Decision Function: {round( detection[1],2)}' \
-                            f'\n\n Overlap: {overlap}% der eingegebenen Wörter sind in den Trainingsdaten enthalten.'
 
-                elif 'processing-button' in changed_id:
-                    dropwords_value = [x.strip().lower() for x in dropwords_value.split(",")]
-                    self.process(dropwords=dropwords_value)
-                    return f'\n\n Feature processing abgeschlossen. \nDatei gespeichert als {config.PROCESSED_DATA}.\n' \
-                           f'Entfernte Features: {dropwords_value}'
+                    return  f'\n\n Klassifiziert als: {detection[0]}'
+                            #f'\n\n Decision Function: {round( detection[1],2)}' \
+                            #f'\n\n Overlap: {overlap}%'
 
-                elif 'model-button' in changed_id:
-                    self.train_model()
-                    return f'\n\n Training abgeschlossen.'
+
+            return f'\n\n Klassifiziert als: -'
+
+
+        @app.callback(
+
+            Output('textarea-status-output2', 'children'),
+
+            [Input('processing-button', 'n_clicks'),
+             Input('model-button', 'n_clicks'),
+             Input('input-dropwords', 'value'),],
+
+        )
+        def update_output2(btn1, btn2, dropwords_value):
+
+            changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+
+            if 'processing-button' in changed_id:
+
+                dropwords_value = [x.strip().lower() for x in dropwords_value.split(",")]
+                self.process(dropwords=dropwords_value)
+                return f'\n\n Feature processing abgeschlossen. \nDatei gespeichert als {config.PROCESSED_DATA}.\n' \
+                       f'Entfernte Features: {dropwords_value}'
+
+            elif 'model-button' in changed_id:
+                self.train_model()
+                return f'\n\n Training abgeschlossen.\n\n' \
+                       f'Accuracy:\t {self.accuracy_score}\n' \
+                       f'Precision:\t {self.precision_score}\n' \
+                       f'Recall:\t{self.recall_score}\n' \
+                       f'F1-Score:\t {self.f1_score}'
+
 
         app.run_server(debug=True)
+
+
+
 
 
 
